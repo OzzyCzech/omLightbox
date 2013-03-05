70e08744ac37c5529e7c099356ac5a9e675c7e6f
Roman Ožana <roman@wikidi.com>
2013-03-05 13:40:19 +0100

    Add package json

---
+++ omSlimBox.php
@@ -0,0 +1,10 @@
+/**
+ * Plugin Name: omSlimBox
+ * Plugin URI: http://www.omdesign.cz
+ * Description: SlimBox2 for Wordpress
+ * Version: 1.0
+ * Author: Roman Ožana
+ * Author URI: http://www.omdesign.cz/kontakt
+ *
+ * @author Roman Ozana <ozana@omdesign.cz>
+ */
