562b4e54864348ca31dd12411949e221e239fb88
Roman Ožana <roman@wikidi.com>
2013-03-05 15:00:31 +0100

    Fix omSlimBox config

--- omSlimBox.php
+++ omSlimBox.php
@@ -25,33 +25,27 @@ class omSlimBox {
 	}
 
 	public function __construct() {
-		if (self::$js) {
-			add_action('wp_print_scripts', array(&$this, 'enqueue_scripts'));
-		}
-
-		if (self::$css) {
-			add_action('wp_print_styles', array(&$this, 'enqueue_styles'));
-		}
-
-		if (self::$content) {
-			add_filter('the_content', array(&$this, 'content'), 99);
-		}
+		add_action('wp_print_scripts', array(&$this, 'enqueue_scripts'));
+		add_action('wp_print_styles', array(&$this, 'enqueue_styles'));
+		add_filter('the_content', array(&$this, 'content'), 99);
 	}
 
 	public function enqueue_scripts() {
-		if (is_admin()) return;
+		if (!self::$js || is_admin()) return;
 		wp_enqueue_script(
 			'wp-slimbox', plugins_url('slimbox/js/slimbox2.js', __FILE__), array('jquery'), self::$version, true
 		);
 	}
 
 	public function enqueue_styles() {
-		if (is_admin() || is_feed()) return;
+
+		if (!self::$css || is_admin() || is_feed()) return;
 		wp_enqueue_style('wp-slimbox', plugins_url('slimbox/css/slimbox2.css', __FILE__), false, self::$version);
 	}
 
 
 	public function content($content) {
+		if (!self::$content) return $content;
 		$pattern = "/(<a(?![^>]*?rel=['\"]lightbox.*)[^>]*?href=['\"][^'\"]+?\.(?:bmp|gif|jpg|jpeg|png)\?{0,1}\S{0,}['\"][^\>]*)>/i";
 		$replacement = '$1 rel="' . (get_the_ID() ? 'lightbox-' . get_the_ID() : 'lightbox') . '">';
 		return preg_replace($pattern, $replacement, $content);
